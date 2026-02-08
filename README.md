3acec7d4e85d14619b3514ebcc0fc3393d83346c
Golam Ahmad Saba <golamahmadsaba@gmail.com>
2026-02-08 19:38:05 +0600

    Updated calculator project

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# Simple Calculator in C
+# Calculator in C
 
-This is a **simple calculator program** written in C. It allows the user to perform basic arithmetic operations on two numbers: addition, subtraction, multiplication, and division.
+This is a **calculator program** written in C. It allows the user to perform basic arithmetic operations : addition, subtraction, multiplication, and division.
 
 ---
 
 ## Features
 
-* Addition of two numbers
+* Addition of many numbers as your wish
 * Subtraction of two numbers
-* Multiplication of two numbers
+* Multiplication of many numbers as your wish
 * Division of two numbers (checks for division by zero)
 * User-friendly menu interface
 
@@ -31,6 +31,7 @@ gcc calculator.c -o calculator
 
 4. Follow the on-screen menu:
 
+* Enter your name
 * Enter your choice (1-4) for the operation
 * Enter the two numbers
 * View the result
@@ -40,15 +41,39 @@ gcc calculator.c -o calculator
 ## Example
 
 ```
-Calculator Menu for two numbers:
-1. Add
-2. Subtract
-3. Multiply
-4. Divide
+What's your name?
+Golam Ahmad Saba
+Hello,Golam Ahmad Saba
+.
+This is Calculator Menu. What do you want to do ,Golam Ahmad Saba
+?
+
+1. For addition, enter 1
+2. For substraction ,enter 2
+3. For multiplication, enter 3
+4. For division , enter 4
 Enter your choice: 1
-Enter first number: 5
-Enter second number: 3
-Summation of 5 and 3 is: 8
+How many numbers: 3
+Enter 1 number:25
+Enter 2 number:15
+Enter 3 number:10
+The summation is : 50
+Do you want do any calculation ? Enter Y for yes and N for no : 
+Y
+
+
+This is Calculator Menu. What do you want to do ,Golam Ahmad Saba
+?
+1. For addition, enter 1
+2. For substraction ,enter 2
+3. For multiplication, enter 3
+4. For division , enter 4
+Enter your choice: 2
+Enter 1st Number :50
+Enter 2nd Number :12
+Subtraction of 50 and 12 is : 38
+Do you want do any calculation ? Enter Y for yes and N for no : 
+N
 ```
 
 ---
--- calculator.c
+++ calculator.c
@@ -1,41 +1,121 @@
 #include <stdio.h>
 
 int main() {
-  int choice;
-  int a, b;
-    
-        printf("\nCalculator Menu for two numbers:\n");
-        printf("1. Add\n");
-        printf("2. Subtract\n");
-        printf("3. Multiply\n");
-        printf("4. Divide\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-        printf("Enter first  number: ");
-         scanf("%d",&a);
-         printf("Enter  Second number:");
-         scanf("%d",&b);
-     
-        switch (choice) {
-            case 1:
-                printf("Summation of %d and %d is : %d\n",a,b, a + b);
-                break;
-            case 2:
-                printf("Subtraction of %d and %d is : %d\n",a,b, a - b);
-                break;
-            case 3:
-                printf("Multiplication of %d and %d is : %d\n",a,b, a * b);
-                break;
-            case 4:
-                if (b != 0)
-                    printf("Division of %d and %d is : %d\n",a,b, a / b);
-                else
-                    printf("Error!\n");
-                break;
-            default:
-                printf("Invalid choice! Try again.\n");
-        }
-   
 
-    return 0;
+
+char name[100];   
+printf("What's your name?\n");
+fgets(name,sizeof(name), stdin);
+printf("Hello,%s.\n",name);
+
+
+
+
+char confirmation='Y';
+do{
+
+
+  int choice;
+  int a,b;
+  printf("This is Calculator Menu. What do you want to do ,%s?\n",name);
+  printf("1. For addition, enter 1\n");
+  printf("2. For substraction ,enter 2\n");
+  printf("3. For multiplication, enter 3\n");
+  printf("4. For division , enter 4\n");
+  printf("Enter your choice: ");
+  scanf(" %d", &choice);
+
+
+
+
+if(choice==1){
+int count,i;
+printf("How many numbers: ");
+scanf(" %d",&count);
+int numbers[count],summation=0;
+
+for(i=1;i<=count;i++)
+ {
+ printf("Enter %d number:",i);
+ scanf(" %d",&numbers[i-1]);
+ }
+
+for (i=1;i<=count;i++){
+summation=summation+numbers[i-1];
+}
+
+printf("The summation is : %d\n",summation);
+
+}
+
+
+
+
+else if(choice==2){
+int num1,num2;
+printf("Enter 1st Number :");
+scanf(" %d",&num1);
+printf("Enter 2nd Number :");
+scanf(" %d",&num2);
+printf("Subtraction of %d and %d is : %d\n",num1,num2,num1-num2);
+}
+
+
+
+
+else if (choice==3){
+int count,i;
+printf("How many numbers: ");
+scanf(" %d",&count);
+int numbers[count],multiplication=1;
+
+for(i=1;i<=count;i++)
+ {printf("Enter %d number:",i);
+ scanf("%d",&numbers[i-1]);}
+
+for (i=1;i<=count;i++){
+multiplication=multiplication*numbers[i-1];
+}
+
+printf("The multiplication is : %d\n",multiplication);            
+}
+
+
+
+
+
+else if (choice==4){
+int num1,num2;
+printf("Enter 1st Number :");
+scanf(" %d",&num1);
+printf("Enter 2nd Number :");
+scanf(" %d",&num2);
+
+if (num2!= 0)
+ printf("Division of %d and %d is : %d\n",num1,num2, num1/num2 );
+else
+printf("Error!\n");
+}
+
+
+
+
+
+else{
+   printf("Invalid choice! Try again.\n");
+}
+
+
+
+
+printf("Do you want do any calculation ? Enter Y for yes and N for no : \n");   
+scanf(" %c", &confirmation);
+
+
+
+}while(confirmation=='Y');
+
+ 
+ 
+ return 0;
 }
